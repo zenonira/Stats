2918f836358f1d7b4dd8f71e1ff19c96ebaf0e84
apcs <apcs@mvhs-u528ap03.dcsdk12.org>
2013-09-19 08:47:34 -0600

    Min and Max

---
+++ Stats.java
@@ -0,0 +1,43 @@
+// max
+
+// min
+
+// median
+
+// quartile 1
+
+// quartile 3
+
+// mode
+
+// standard deviation
+
+class Stats {
+	public static void main(String[] args) {
+		int [] a = {1, 2, 3, 4, 5, 6};
+		//max(a);
+		min(a);
+	}
+
+	public static int max(int [] a) {
+		int max = a[0];
+		for (int i = 0; i < a.length; i++) {
+			if (max <= a[i]) {
+				max = a[i];
+			}
+		}
+		System.out.println(max);
+		return max;
+	}
+
+	public static int min(int [] a) {
+		int min = a[0];
+		for (int i = 0; i < a.length; i++) {
+			if (min >= a[i]) {
+				min = a[i];
+			}
+		}
+		System.out.println(min);
+		return min;
+	}
+}
