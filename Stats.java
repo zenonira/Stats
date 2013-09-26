e0c1398ad77b23a1d1683a8ade29e7a6fdb49995
Ryan Zenoni <Ryan@Lisa-Zenonis-MacBook-Pro.local>
2013-09-25 22:48:37 -0600

    Finished

--- Stats.java
+++ Stats.java
@@ -1,6 +1,6 @@
 class Stats {
 	public static void main(String[] args) {
-		int [] a = {1, 2, 3, 4, 5, 6, 4, 5, 6, 5};
+		int [] a = {1, 2, 3, 4, 5};
 		print(max(a));
 		print(min(a));
 		print(mean(a));
@@ -17,87 +17,102 @@ class Stats {
 
 	public static int max(int [] a) {
 		int max = a[0];
-		for (int i = 0; i < a.length; i++) {
+		for (int i = 1; i < a.length; i++) {
 			if (max <= a[i]) {
 				max = a[i];
 			}
 		}
-		// System.out.println("The max is: " + max);
 		return max;
 	}
 
 	public static int min(int [] a) {
 		int min = a[0];
-		for (int i = 0; i < a.length; i++) {
+		for (int i = 1; i < a.length; i++) {
 			if (min >= a[i]) {
 				min = a[i];
 			}
 		}
-		//System.out.println("The min is: " + min);
 		return min;
 	}
 
 	public static double mean(int [] a) {
+		double sum = 0.0;
 		double mean = 0.0;
 		for (int i =0; i<a.length; i++) {
-			mean = mean + a[i];
+			sum += a[i];
 		}
-		mean = mean / a.length;
-		//System.out.println("The mean is: " + mean);
+		mean = sum / a.length;
 		return mean;
 	}
 
 	public static double median(int [] a) {
-		int all = 0;
-		double median = 0.0;
-		for (int i = 0; i<a.length; i++) {
-			all++;
+		int length = a.length;
+		double med = 0.0;
+		if (length % 2 == 1) {
+			length++;
+			med = a[length/2 - 1];
+			return med;
 		}
-		if (all % 2 == 0) {
-			all = all / 2;
-			median = (a[all] + a[all - 1]) / 2.0;
+		if (length == 1) {
+			med = a[0];
+			return med;
 		}
-		if (all % 2 == 1) {
-			all = all / 2;
-			median = a[all];
-		}
-		//System.out.println("The median is: " + median);
-		return median;
+		length = length / 2;
+		med = (a[length] + a[length - 1]) / 2.0;
+		return med;
 	}
 
 	public static double quartile1(int  [] a) {
-		int length = 0;
-		double median = 0.0;
-		for (int i = 0; i<a.length; i++) {
-			length++;
+		int length = a.length;
+		double med = 0.0;
+		double quart = 0.0;
+		if (length == 1) {
+			quart = a[0];
+			return quart;
 		}
 		if (length % 2 == 0) {
-			length = length / 2;
-			median = (a[length] + a[length - 1]) / 4.0;
+			//Even
+			int [] b = new int [length/2];
+			for (int i = 0; i < length/2 ; i++) {
+				b[i] = a[i];
+			}
+			quart = median(b);
+			return quart;
 		}
-		if (length % 2 == 1) {
-			length = length / 4;
-			median = a[length];
+		else { //Odd
+			int [] b = new int [(length-1)/2];
+			for (int i = 0; i < (length-1)/2; i++) {
+				b[i]=a[i];
+			}
+			quart = median(b);
+			return quart;
 		}
-		//System.out.println("Quartile 1 equals: " + median);
-		return median;
 	}
 	public static double quartile3(int [] a) {
-		int length = 0;
-		double median = 0.0;
-		for (int i = 0; i<a.length; i++) {
-			length++;
+		int length = a.length;
+		double med = 0.0;
+		double quart = 0.0;
+		if (length == 1) {
+			quart = a[0];
+			return quart;
 		}
 		if (length % 2 == 0) {
-			length = length / 2;
-			median = (a[length] + a[length - 1]) / 4.0 * 3.0;
+			//Even
+			int [] b = new int [length/2];
+			for (int i = 0; i<length/2; i++) {
+				b[i] = a[length/2 + i];
+			}
+			quart = median(b);
+			return quart;
+
+		} else { //Odd
+			int [] b = new int [(length-1)/2];
+			for (int i=0; i<(length-1)/2; i++) {
+				b[i] = a[(length-1)/2 + 1 + i];
+			}
+			quart = median(b);
+			return quart;
 		}
-		if (length % 2 == 1) {
-			length = length / 4 * 3;
-			median = a[length];
-		}
-		//System.out.println("Quartile 3 equals: " + median);
-		return median;
 	}
 	public static int mode(int [] a) {
 		int num = 0;
@@ -117,8 +132,6 @@ class Stats {
 				}
 			}
 		}
-		//System.out.println("The mode is: " + great);
-		//System.out.println("The mode is repeated " + value + " times");
 		return great;
 	}
 	public static double standardDeviation(int [] a) {
@@ -129,9 +142,8 @@ class Stats {
 		for (int i=0; i<a.length; i++) {
 			sum += (a[i] - mean) * (a[i] - mean);
 		}
-		divide = sum/(a.length - 1);
+		divide = sum/a.length - 1;
 		standard = Math.sqrt(divide);
-		//System.out.println("The standard deviation is: " + standard);
 		return standard;
 	}
 }
